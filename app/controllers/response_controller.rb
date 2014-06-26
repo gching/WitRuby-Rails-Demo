d6486e099bf25aaf5faf4f35a1af82439f3b10d5
Gavin Ching <gavinchingy@gmail.com>
2014-06-26 14:27:43 -0700

    Added helper in response controller to properly set the youtube link given the results

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -1,5 +1,6 @@
 
 class ResponseController < ApplicationController
+  helper_method :youtube_link_decider
   def show
     @link = flash[:link]
   end
@@ -12,20 +13,29 @@ class ResponseController < ApplicationController
     ## Make API call only if the length is between not zero or greater than 256
     if under_or_over_length
       flash[:alert] = "You didn't say anything or the message length is greater than 256."
+      flash[:link] = "wow"
     else
       results = $wit_session.send_message(params[:message])
-      if results.confidence < 0.5
-        link = "nope1"
-      else
-        case results.intent
-        when "greetings"
-          link = '0Bmhjf0rKe8'
-        else
-          link = "nope2"
-        end
-      end
-      flash[:link] = link
+      flash[:link] = youtube_link_decider(results)
     end
     redirect_to action: "show"
+  end
+
+
+  ## Assists in returning the correct youtube link given the wit.ai results
+  ## Returns the string of the youtube link
+  def youtube_link_decider(results)
+    if results.confidence < 0.5
+      link = "nope1"
+    else
+      case results.intent
+      when "greetings"
+        link = '0Bmhjf0rKe8'
+      else
+        link = "nope2"
+      end
+    end
+
+    return link
   end
 end
